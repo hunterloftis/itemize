ef8e19702d7b6ff87296066fa562730b47401748
Hunter Loftis <hunter@hunterloftis.com>
2017-02-19 16:47:06 -0500

    update examples

--- examples/hackernews.js
+++ examples/hackernews.js
@@ -1,9 +1,10 @@
 const itemize = require('..')
+const urls = itemize('https://news.ycombinator.com', { depth: 2 })
 
-list()  // Get a quick Hacker News sitemap
+list()
 
+// Get a quick Hacker News sitemap
 async function list() {
-  const urls = itemize('https://news.ycombinator.com', { depth: 2 })
   while (!urls.done()) {
     console.log(await urls.next())
   }
